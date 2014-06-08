67d0b7d713d7e35b3843b48dffaaf15a91bc813b
ForNeVeR <fvnever@gmail.com>
2014-06-08 18:13:34 +0700

    Application: fetching and returning the feed content.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,9 @@ package controllers
 import play.api._
 import play.api.mvc._
 import models.Feed
+import com.rometools.fetcher.impl.{HttpURLFeedFetcher, HashMapFeedInfoCache}
+import java.net.URL
+import play.twirl.api.Xml
 
 object Application extends Controller {
 
@@ -13,10 +16,18 @@ object Application extends Controller {
     }).toMap
   }
 
+  val feedInfoCache = HashMapFeedInfoCache.getInstance()
+  val fetcher = new HttpURLFeedFetcher(feedInfoCache)
+
   def feed(feedName: String) = Action {
     feeds.get(feedName) match {
-      case Some(feed) => Ok("ok")
-      case None => NotFound("not found")
+      case Some(feed) =>
+        val url = new URL(feed.url)
+        val content = fetcher.retrieveFeed(url)
+        // TODO: Filter the content.
+        // TODO: Serve the content as proper XML.
+        Ok(Xml(content.toString))
+      case None => NotFound("Feed not found.")
     }
   }
 
