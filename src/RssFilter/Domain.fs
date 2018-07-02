317dd1eea3c247fea74d34cead3aef5b68c1119b
Friedrich von Never <friedrich@fornever.me>
2018-07-02 23:02:00 +0700

    Implement main logic in F#

---
+++ src/RssFilter/Domain.fs
@@ -0,0 +1,34 @@
+module RssFilter.Domain
+
+open System
+open System.Text.RegularExpressions
+open System.Threading
+open System.Threading.Tasks
+open System.Xml
+open System.Xml.Linq
+open System.Xml.XPath
+
+open FSharp.Control.Tasks.ContextInsensitive
+
+type Feed = XDocument
+
+let readFeed(uri : Uri) (ct : CancellationToken) : Task<Feed> =
+    task {
+        use stream = XmlReader.Create(uri.ToString())
+        return! XDocument.LoadAsync(stream, LoadOptions.None, ct)
+    }
+
+let private getItemTitle(item : XElement) =
+    item.Element(XName.Get "title").Value
+
+let filterFeed (itemTitle : Regex) (feed : Feed) : Feed =
+    let newFeed = XDocument feed
+    let items = newFeed.XPathSelectElements("/rss/channel/item")
+    items
+    |> Seq.filter(getItemTitle >> itemTitle.IsMatch)
+    |> ResizeArray
+    |> Seq.iter(fun item -> item.Remove())
+    newFeed
+
+let serialize(feed : Feed) : string =
+    feed.ToString(SaveOptions.DisableFormatting)
---
+++ src/RssFilter/Server.fs
@@ -0,0 +1,89 @@
+module RssFilter.Server
+
+open System
+open System.IO
+
+open System.Text.RegularExpressions
+open System.Xml.Linq
+open Giraffe
+open FSharp.Control.Tasks.ContextInsensitive
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Options
+open Microsoft.Extensions.DependencyInjection
+
+[<CLIMutable>]
+type FeedSettings =
+    { name : string
+      url : string
+      titleFilter : string }
+
+[<CLIMutable>]
+type RssFilterSettings =
+    { rss : FeedSettings seq }
+
+let private notFound = setStatusCode 404 >=> text "Not Found"
+
+let private getFeedSettings (ctx : HttpContext) feedName =
+    let settings = ctx.GetService<IOptions<RssFilterSettings>>()
+    settings.Value.rss
+    |> Seq.filter(fun feed -> feed.name = feedName)
+    |> Seq.tryHead
+
+let private rss(content : Domain.Feed) : HttpHandler =
+    setHttpHeader "Content-Type" "application/rss+xml"
+    >=> setBodyFromString(Domain.serialize content)
+
+let private feedHandler(feedName : string) : HttpHandler =
+    fun next (ctx : HttpContext) ->
+        let feed = getFeedSettings ctx feedName
+        task {
+            match feed with
+            | Some feed ->
+                let! content = Domain.readFeed (Uri feed.url) ctx.RequestAborted
+                let filteredContent = Domain.filterFeed (Regex feed.titleFilter) content
+                return! rss filteredContent next ctx
+            | None ->
+                return! notFound next ctx
+        }
+
+let private webApp =
+    choose [
+        GET >=>
+            choose [
+                routef "/feed/%s" feedHandler
+            ]
+        notFound ]
+
+let private errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+let private configureApp(app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+    (match env.IsDevelopment() with
+    | true  -> app.UseDeveloperExceptionPage()
+    | false -> app.UseGiraffeErrorHandler errorHandler)
+        .UseGiraffe(webApp)
+
+let private configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+let private configureLogging (builder : ILoggingBuilder) =
+    let filter (l : LogLevel) = l.Equals LogLevel.Error
+    builder.AddFilter(filter).AddConsole().AddDebug() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    let contentRoot = Directory.GetCurrentDirectory()
+    WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(contentRoot)
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .ConfigureLogging(configureLogging)
+        .Build()
+        .Run()
+    0
